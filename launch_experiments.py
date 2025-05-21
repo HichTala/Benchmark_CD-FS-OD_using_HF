a49edb990130f32c3e5dd38b03e747f8da9b7600
HichTala <98521878+HichTala@users.noreply.github.com>
2025-05-21 10:53:23 +0200

    Add classic detection

--- launch_experiments.py
+++ launch_experiments.py
@@ -1,6 +1,5 @@
 import argparse
 import json
-import os.path
 import subprocess
 
 slurm_script = r"""#!/bin/bash
@@ -37,6 +36,9 @@ module load pytorch-gpu/py3/2.5.0 # load modules
 export PYTHONUSERBASE=$WORK/.local
 export WANDB_MODE=offline
 export NO_ALBUMENTATIONS_UPDATE=1
+export HF_HUB_OFFLINE=1
+export HF_DATASETS_OFFLINE=1
+export TRANSFORMERS_OFFLINE=1
 set -x # activate echo of launched commands
 
 srun python -u run_object_detection.py{command}"""
@@ -47,17 +49,7 @@ def get_args_parser():
 
     parser.add_argument('--config', type=str, default="configs/models/diffusiondet_dota.json")
     parser.add_argument('--dataset_names', nargs='+', default=["detection-datasets/coco"])
-    parser.add_argument('--seed', nargs='+', default=["1338"])
-    parser.add_argument('--shots', nargs='+', default=["10"])
     parser.add_argument('--output_dir', type=str, default="diffusiondet")
-
-    parser.add_argument('--freeze_mode', type=bool, default=False)
-    parser.add_argument('--freeze_modules', type=str)
-    parser.add_argument('--freeze_at', type=str)
-
-    parser.add_argument('--use_lora', type=bool, default=False)
-    parser.add_argument('--over_lora', type=bool, default=False)
-    parser.add_argument('--lora_ranks', nargs='+', default=["8"])
 
     parser.add_argument('--exec_type', type=str, default="slurm")
 
@@ -75,10 +67,10 @@ def build_cmd(config):
 
 def submit_job(cmd, exec_type, **kwargs):
     if exec_type == "slurm":
-        formated_script = slurm_script.format(job_name=kwargs['shot'] + 'nl' + kwargs['seed'], command=cmd)
+        formated_script = slurm_script.format(job_name=kwargs['dataset'], command=cmd)
         with open('launchers/automatic_launcher.slurm', 'w') as f:
             f.write(formated_script)
-        print('job_name', kwargs['shot'] + 'nl' + kwargs['seed'])
+        print('job_name', kwargs['dataset'])
         return subprocess.call(['sbatch', 'launchers/automatic_launcher.slurm'])
     elif exec_type == "python":
         cmd = f"python run_object_detection.py{cmd}"
@@ -90,73 +82,20 @@ def main(args):
     with open(args.config) as f:
         config = json.load(f)
 
-    if not args.freeze_modules:
-        args.freeze_modules = ['', 'backbone', 'backbone', 'bias', 'norm']
-    if not args.freeze_at:
-        args.freeze_at = ['', '0', 'half', '', '']
-
     for dataset_name in args.dataset_names:
-        for shot in args.shots:
-            for seed in args.seed:
-                if args.freeze_mode:
-                    for freeze_modules, freeze_at in zip(args.freeze_modules, args.freeze_at):
-                        output_dir = f"runs/{args.output_dir}/{dataset_name.rstrip('/').split('/')[-1]}/{shot}/seed_{seed}/"
+        output_dir = f"runs/{args.output_dir}/{dataset_name.rstrip('/').split('/')[-1]}/classic_detection"
 
-                        if len(freeze_modules) == 0:
-                            output_dir += "full_finetuning"
-                        output_dir += f"{freeze_modules}-{freeze_at}" if freeze_at != '0' else f"{freeze_modules}-full"
-                        if output_dir[-1] == '-':
-                            output_dir = output_dir[:-1]
+        config["dataset_name"] = dataset_name
+        config["output_dir"] = output_dir
+        config["logging_strategy"] = "epoch"
+        config["evaluation_strategy"] = "epoch"
+        config["save_strategy"] = "epoch"
 
-                        config['freeze_modules'] = freeze_modules
-                        config['freeze_at'] = freeze_at
-
-                        config["dataset_name"] = dataset_name
-                        config["seed"] = seed
-                        config["shots"] = shot
-                        config["output_dir"] = output_dir
-
-                        cmd = build_cmd(config)
-                        result = submit_job(cmd, exec_type=args.exec_type)
-                        # if result.returncode != 0:
-                        #     print(f"Error running command: python run_object_detection.py{cmd}")
-                        #     return
-                else:
-                    output_dir = f"runs/{args.output_dir}/{dataset_name.rstrip('/').split('/')[-1]}/{shot}/nolora/{seed}"
-
-                    logging_steps = {'50': 970, '10':100, '5': 50, '1':10}
-
-                    config["dataset_name"] = dataset_name
-                    config["seed"] = seed
-                    config["shots"] = shot
-                    config["output_dir"] = output_dir
-                    config["eval_steps"] = logging_steps[shot]
-                    config["save_steps"] = logging_steps[shot]
-
-                    if args.use_lora:
-                        config["use_lora"] = True
-                        for rank in args.lora_ranks:
-                            config["lora_rank"] = rank
-                            config["output_dir"] = f"runs/{args.output_dir}/{dataset_name.rstrip('/').split('/')[-1]}/{shot}/lora/{seed}/{rank}"
-
-                            if args.over_lora:
-                                if os.path.isfile(f"runs/{args.output_dir}/{dataset_name.rstrip('/').split('/')[-1]}/{shot}/nolora/{seed}/trainer_state.json"):
-                                    with open(f"runs/{args.output_dir}/{dataset_name.rstrip('/').split('/')[-1]}/{shot}/nolora/{seed}/trainer_state.json") as f:
-                                        trainer_state = json.load(f)
-                                    config['model_name_or_path'] = trainer_state['best_model_checkpoint']
-                                    config["output_dir"] = f"runs/{args.output_dir}/{dataset_name.rstrip('/').split('/')[-1]}/{shot}/overlora/{seed}/{rank}"
-
-                            cmd = build_cmd(config)
-                            result = submit_job(cmd, exec_type=args.exec_type, seed=seed, shot=shot)
-                            if result != 0:
-                                print(f"Error running command: python run_object_detection.py{cmd}")
-                                return
-                    else:
-                        cmd = build_cmd(config)
-                        result = submit_job(cmd, exec_type=args.exec_type, seed=seed, shot=shot)
-                        if result != 0:
-                            print(f"Error running command: python run_object_detection.py{cmd}")
-                            return
+        cmd = build_cmd(config)
+        result = submit_job(cmd, exec_type=args.exec_type, dataset=dataset_name)
+        if result != 0:
+            print(f"Error running command: python run_object_detection.py{cmd}")
+            return
 
 
 if __name__ == "__main__":
